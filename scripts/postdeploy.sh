7d88b8b700d7247b4f15b68c0d15b68635f07952
Haitham Athamneh <himoath@gmail.com>
2019-09-15 21:24:56 +0300

    fix pathes in deployment script

--- scripts/postdeploy.sh
+++ scripts/postdeploy.sh
@@ -4,9 +4,9 @@ cd ~/repositories/qudsinfo-wp-theme/
 updatedFiles=$(git log --format="%H" -n 2 | xargs git diff --name-only)
 for file in $updatedFiles
 do
-  cp $file $DEPLOYPATH/$file
+  rsync -avzh $file $DEPLOYPATH/$file
 done
 
 cd layout
 ~/bin/npm run build
-cp -r dist $DEPLOYPATH/layout/dist
+rsync -avzh dist $DEPLOYPATH/layout/dist
