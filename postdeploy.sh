8468a06dd1f5af3800758addd22dee4f7b8af0a9
Haitham Athamneh <himoath@gmail.com>
2019-09-15 22:22:27 +0300

    fix the dist path

--- postdeploy.sh
+++ postdeploy.sh
@@ -5,11 +5,11 @@ for file in $updatedFiles
 do
   if [ -f "./$file" ]
   then
-    rsync -avzhR $file $DEPLOYPATH/$file
+    rsync -avhR $file $DEPLOYPATH/$file
   fi
 done
 
 cd ~/repositories/qudsinfo-wp-theme/layout
 ~/bin/npm install
 ~/bin/npm run css:build
-rsync -avzhR dist/ $DEPLOYPATH/layout/dist
+rsync -avh dist/ $DEPLOYPATH/layout/dist
