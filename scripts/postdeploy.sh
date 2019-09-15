06b6ef4223ccc2bde0531ba495a73b3904ce7a5e
Haitham Athamneh <himoath@gmail.com>
2019-09-15 21:02:31 +0300

    deployment script

---
+++ scripts/postdeploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+updatedFiles=$(git log --format="%H" -n 2 | xargs git diff --name-only)
+for file in $updatedFiles
+do
+  cp $file $DEPLOYPATH$file
+done
+
+cd layout
+npm run build
+cp -r dist $DEPLOYPATH/layout/dist
