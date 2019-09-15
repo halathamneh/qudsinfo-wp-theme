4834376b4e67985b43a93222262eae402a5ec871
Haitham Athamneh <himoath@gmail.com>
2019-09-15 21:59:26 +0300

    add parcel to package.json

--- postdeploy.sh
+++ postdeploy.sh
@@ -5,11 +5,11 @@ for file in $updatedFiles
 do
   if [ -f "./$file" ]
   then
-    rsync -aR $file $DEPLOYPATH/$file
+    rsync -avzhR $file $DEPLOYPATH/$file
   fi
 done
 
 cd ~/repositories/qudsinfo-wp-theme/layout
 ~/bin/npm install
 ~/bin/npm run css:build
-rsync -aR dist $DEPLOYPATH/layout/dist
+rsync -avzhR dist $DEPLOYPATH/layout/dist
