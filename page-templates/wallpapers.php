2949eb8024ac2eac29616f5ef8740af31f9a0bf7
haitham <himoath@gmail.com>
2019-11-02 21:53:25 +0200

    remove wallpaper type

--- page-templates/wallpapers.php
+++ page-templates/wallpapers.php
@@ -36,7 +36,6 @@ $pagination_query = $post_query;
             <div class="page-heading">
                 <h1 class="text-light"><?= get_the_title() ?></h1>
                 <ul class="nav nav-pills d-flex justify-content-center" id="pills-tab" role="tablist">
-                    <li class="nav-label"><?php __("Wallpaper type", 'qi-theme') ?>:</li>
                     <li class="nav-item">
                         <a href="/wallpapers/?type=desktop"
                            class="nav-link <?= $type == 'desktop' ? 'active' : '' ?>"><?= __('Desktop', 'qi-theme') ?></a>
