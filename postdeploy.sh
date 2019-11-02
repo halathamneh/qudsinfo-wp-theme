dc795feda6d8be0b6547835c34bad07d40fa6cf7
haitham <himoath@gmail.com>
2019-11-02 21:42:06 +0200

    fix post deploy

--- postdeploy.sh
+++ postdeploy.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-
+updateLayout=0
 updatedFiles=$(git log --format="%H" -n 2 | xargs git diff --name-only)
-for file in $updatedFiles
-do
-  if [ -f "./$file" ]
-  then
-    rsync -avh -I $file $DEPLOYPATH/$file
+for file in $updatedFiles; do
+  if [ -f "./$file" ]; then
+    if [[ $file == layout* ]]; then
+      updateLayout=1
+    fi
+    rsync -avh -I "$file" "$DEPLOYPATH/$file"
   fi
 done
 
-cd ~/repositories/qudsinfo-wp-theme/layout
-~/bin/npm install
-~/bin/npm run css:build
-rsync -avh dist/ $DEPLOYPATH/layout/dist
+if [[ $updateLayout == 1 ]]; then
+  cd ~/repositories/qudsinfo-wp-theme/layout || exit
+  ~/bin/npm install
+  ~/bin/npm run css:build
+  rsync -avh -I dist/ "$DEPLOYPATH/layout/dist"
+fi
